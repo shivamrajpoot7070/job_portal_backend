8aa65123623f8c0f0759aff5365fb8820cd00ff9
shivam kumar <shivamrajpoot7070@gmail.com>
2024-12-23 23:58:28 +0530

    v2.0

--- server.js
+++ server.js
@@ -16,15 +16,21 @@ connectDB();
 
 const app = express();
 
-// Enable CORS for all responses
-app.use(cors());
+// Configure CORS to allow credentials and specify the origin
+const corsOptions = {
+  origin: 'https://jobportalshivam.netlify.app', // Specify the allowed origin
+  credentials: true  // Allow credentials such as cookies, authorization headers etc.
+};
+
+// Apply CORS with the specified options
+app.use(cors(corsOptions));
 
 // Middleware to parse JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Define routes
+// Define API routes
 app.use("/api/v1/user", UserRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
@@ -33,7 +39,7 @@ app.use("/api/v1/application", applicationRoute);
 // Set the port from environment variables or use default
 const PORT = process.env.PORT || 3000;
 
-// Start the server
+// Start the server and log the running port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
