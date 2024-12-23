ef60d8b566899dd9c68bab5462b1c290e166f523
shivam kumar <shivamrajpoot7070@gmail.com>
2024-12-23 23:49:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,36 +7,33 @@ import UserRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-//import applicationRouter from "./routes/application.route.js";
 
+// Initialize environment variables
 dotenv.config();
-connectDB(); // made in utils
+
+// Connect to the database
+connectDB();
 
 const app = express();
 
-app.use(express.json()); //  middleware for coming post data
-app.use(express.urlencoded({ extended: true })); // middleware for coming post data
+// Enable CORS for all responses
+app.use(cors());
+
+// Middleware to parse JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173", // Localhost development
-      "https://ppsk4j18-5173.inc1.devtunnels.ms", // Devtunnel URL
-    ],
-    credentials: true, // Required for withCredentials to work
-  })
-);
-
-const PORT = process.env.PORT || 3000;
-
-// api
-
+// Define routes
 app.use("/api/v1/user", UserRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+// Set the port from environment variables or use default
+const PORT = process.env.PORT || 3000;
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
